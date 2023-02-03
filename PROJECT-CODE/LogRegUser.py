5610a88b09b73cadf34923bc240e85cd5408bfa2
Mariusz Perzynski <m.perzyk@interia.pl>
2023-02-03 17:05:32 +0100

    modyf. elements and bcg window

--- PROJECT-CODE/LogRegUser.py
+++ PROJECT-CODE/LogRegUser.py
@@ -1,35 +1,46 @@
-from PyQt5.QtWidgets import * 
-from PySide6.QtWidgets import *
-from PySide6.QtSql import *
-from PyQt5 import QtWidgets, QtGui, QtCore
-
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QFormLayout, QLineEdit, QPushButton 
+from PyQt5 import QtGui, QtWidgets, QtCore
 class MainWindow(QDialog):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
+        layout = QVBoxLayout(self)
+        layout.setAlignment(QtCore.Qt.AlignCenter)
+
         form = QFormLayout()
         self.name_edit = QLineEdit()
-        form.addRow("Nazwa użytkownika:", self.name_edit)
+        form_label = QtWidgets.QLabel("Nazwa użytkownika:")
+        form_label.setStyleSheet("color: green; font-weight: bold; font-size: 18px;")
+
+        form.addRow(form_label, self.name_edit)
         
         self.password_edit = QLineEdit()
-        form.addRow("Hasło:", self.password_edit)
+        password_label = QtWidgets.QLabel("Hasło:")
+        password_label.setStyleSheet("color: green; font-weight: bold; font-size: 18px;")
+        form.addRow(password_label, self.password_edit)
         self.password_edit.setEchoMode(QLineEdit.Password)
         
         self.nip_form = QLineEdit()
-        form.addRow("NIP:", self.nip_form)
-
+        nip_label = QtWidgets.QLabel("NIP:")
+        nip_label.setStyleSheet("color: green; font-weight: bold; font-size: 18px;")
+        form.addRow(nip_label, self.nip_form)
+        layout.addLayout(form)
+        
         self.log_button = QPushButton("Zaloguj się")
         self.reg_button = QPushButton("Zarejestruj się")
-        
-        self.name_edit.setStyleSheet("color: black; font-weight: bold; font-size: 18px; background-color: lightyellow; border: 2px solid green; border-radius: 3px;")
-        self.password_edit.setStyleSheet("color: black; font-weight: bold; font-size: 18px; background-color: lightyellow; border: 2px solid green; border-radius: 3px;")
-        self.nip_form.setStyleSheet("color: black; font-weight: bold; font-size: 18px; background-color: lightyellow; border: 2px solid green; border-radius: 3px;")
-        self.log_button.setStyleSheet("color: white; font-weight: bold; font-size: 20px; background-color: red; border-radius: 3px;")
-        self.reg_button.setStyleSheet("color: white; font-weight: bold; font-size: 20px; background-color: red; border-radius: 3px;")
-        
-        layout = QVBoxLayout(self)
-        layout.addLayout(form)
         layout.addWidget(self.log_button)
         layout.addWidget(self.reg_button)
+        
+        self.name_edit.setStyleSheet("color: black; font-weight: bold; font-size: 18px; background-color: lightyellow; border: 2px solid green; border-radius: 3px; font-family: Arial;")
+        self.password_edit.setStyleSheet("color: black; font-weight: bold; font-size: 18px; background-color: lightyellow; border: 2px solid green; border-radius: 3px; font-family: Arial;")
+        self.nip_form.setStyleSheet("color: black; font-weight: bold; font-size: 18px; background-color: lightyellow; border: 2px solid green; border-radius: 3px; font-family: Arial;")
+        self.log_button.setStyleSheet("color: white; font-weight: bold; font-size: 20px; background-color: red; border-radius: 3px; font-family: Arial;")
+        self.reg_button.setStyleSheet("color: white; font-weight: bold; font-size: 20px; background-color: red; border-radius: 3px; font-family: Arial;")
+        
+        palette = self.palette()
+        palette.setBrush(QtGui.QPalette.ColorGroup.Active, QtGui.QPalette.Window, QtGui.QBrush(QtGui.QPixmap(r"C:\Users\mperz\Desktop\MAIG WAREHOUSE\JPEGEIMAGE\alsn20210928150320120vwuc.jpg")))
+        self.setPalette(palette)
+        
         self.setLayout(layout)
+
         
--- PROJECT-CODE/main.py
+++ PROJECT-CODE/main.py
@@ -3,15 +3,14 @@ from PyQt5.QtWidgets import *
 from PySide6.QtWidgets import *
 from PySide6.QtSql import *
 from PyQt5 import QtWidgets
+import sys
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-    program = QApplication([])
-    window = MainWindow()
     
-    #window.resize(500,400)
-    window.setWindowTitle("MAIG WAREHOUSE v1.0.1")
-    window.setStyleSheet("background-color: lightgray;")
-    
-    window.show()
-    program.exec()
+    app = QtWidgets.QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.resize(500,500)
+    main_window.setWindowTitle("MAIG WAREHOUSE v1.0.1")
+    main_window.show()
+    app.aboutToQuit.connect(app.deleteLater)
+    sys.exit(app.exec())
