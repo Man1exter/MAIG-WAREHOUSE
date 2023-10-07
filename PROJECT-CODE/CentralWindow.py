10d6f9f3b491f65654a809da85cdaf158b977c40
Mariusz Perzynski <m.perzyk@interia.pl>
2023-10-07 11:27:57 +0200

    modf.

--- PROJECT-CODE/CentralWindow.py
+++ PROJECT-CODE/CentralWindow.py
@@ -1,10 +1,8 @@
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QFormLayout, QLineEdit, QPushButton, QHBoxLayout, QMessageBox, QTextEdit, QMenu, QAction, QListWidget
-from PyQt5 import QtGui, QtWidgets, QtCore
-from PyQt5.QtCore import Qt
+from PyQt5 import QtWidgets, QtGui, QtCore
 import sys
 import subprocess
 
-class CentralWindowMain(QDialog):
+class CentralWindowMain(QtWidgets.QDialog):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
@@ -21,7 +19,7 @@ class CentralWindowMain(QDialog):
         
         self.setWindowFlags(QtCore.Qt.WindowCloseButtonHint | QtCore.Qt.WindowMinimizeButtonHint | QtCore.Qt.WindowMaximizeButtonHint)
         if not self.isMaximized():
-          self.showMaximized()
+            self.showMaximized()
         
         pixmap = QtGui.QPixmap(r"C:\Users\mperz\Desktop\MAIG WAREHOUSE\JPEGEIMAGE\New-World-niszczy-GPU.jpg")
         pixmap = pixmap.scaled(self.size(), QtCore.Qt.KeepAspectRatioByExpanding, QtCore.Qt.SmoothTransformation)
@@ -29,39 +27,39 @@ class CentralWindowMain(QDialog):
         palette.setBrush(QtGui.QPalette.ColorGroup.Active, QtGui.QPalette.Window, QtGui.QBrush(pixmap))
         self.setPalette(palette)
         
-        navigation_layout = QHBoxLayout()
+        navigation_layout = QtWidgets.QHBoxLayout()
         button_labels = ['Wystaw Dokument', 'Przegląd Dokumentów', 'Zakupy i Koszty', 'Przegląd Towarów', 'Klienci', 'Reklamacje', 'Raporty i Statystyki', 'Trend i Sezonowość', 'Ustawienia Programu', 'Pomoc']
         
         for label in button_labels:
-            button = QPushButton(label)
+            button = QtWidgets.QPushButton(label)
             button.setCursor(QtCore.Qt.PointingHandCursor)
             navigation_layout.addWidget(button)
             button.setStyleSheet("color: white; font-weight: bold; font-size: 18px; background-color: red; border: 2px solid black; border-radius: 3px; font-family: Arial; margin: 10px; padding: 5px;")
             
         navigation_layout.setAlignment(QtCore.Qt.AlignTop)
         
-        self.todo_listwidget = QListWidget()
+        self.todo_listwidget = QtWidgets.QListWidget()
         self.todo_listwidget.setStyleSheet(
-            "font-weight: bold; font-size: 16px; background-color: lightblue; border: 2px solid black; border-radius: 5px; margin: 0px;"
+            "font-weight: bold; font-size: 16px; background-color: lightblue; border: 2px solid black; border-radius: 5px; margin: 0px; height: 50%;"
         )
         self.todo_listwidget.setContentsMargins(30, 30, 30, 30)
         
-        add_button = QPushButton('Dodaj nowe zadanie')
-        edit_button = QPushButton('Edytuj zadanie')
-        delete_button = QPushButton('Usuń zadanie')
+        add_button = QtWidgets.QPushButton('Dodaj nowe zadanie')
+        edit_button = QtWidgets.QPushButton('Edytuj zadanie')
+        delete_button = QtWidgets.QPushButton('Usuń zadanie')
         
         button_style = (
             "color: white; font-weight: bold; font-size: 16px; background-color: blue; border: 2px solid black; "
-            "border-radius: 5px; margin: 10px; padding: 10px;"
+            "border-radius: 5px; margin: 10px; padding: 10px; width: 50%;"
         )
         
         add_button.setStyleSheet(button_style)
         edit_button.setStyleSheet(button_style)
         delete_button.setStyleSheet(button_style)
         
-        add_button.setCursor(Qt.PointingHandCursor)
-        edit_button.setCursor(Qt.PointingHandCursor)
-        delete_button.setCursor(Qt.PointingHandCursor)
+        add_button.setCursor(QtCore.Qt.PointingHandCursor)
+        edit_button.setCursor(QtCore.Qt.PointingHandCursor)
+        delete_button.setCursor(QtCore.Qt.PointingHandCursor)
         
         add_button.clicked.connect(self.add_task)
         edit_button.clicked.connect(self.edit_task)
@@ -69,58 +67,50 @@ class CentralWindowMain(QDialog):
         
         self.todo_listwidget.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
         self.todo_listwidget.customContextMenuRequested.connect(self.show_context_menu)
-        self.context_menu = QMenu(self)
-        delete_action = QAction('Usuń', self)
+        self.context_menu = QtWidgets.QMenu(self)
+        delete_action = QtWidgets.QAction('Usuń', self)
         delete_action.triggered.connect(self.delete_task)
         self.context_menu.addAction(delete_action)
         
-        self.news_textedit = QTextEdit()
-        self.news_textedit.setStyleSheet(
-            "font-weight: bold; font-size: 16px; background-color: lightblue; border: 2px solid black; border-radius: 5px; padding: 10px;"
-        )
-        self.news_textedit.setContentsMargins(30, 30, 30, 30)
-        self.news_textedit.setPlainText("Najnowsze informacje ze świata")
-        
-        logout_button = QPushButton('Wyloguj Się 👤')
+        logout_button = QtWidgets.QPushButton('Wyloguj Się 👤')
         logout_button.setStyleSheet("color: black; font-weight: bold; font-size: 18px; background-color: yellow; border: 2px solid black; border-radius: 3px; font-family: Arial; margin: 10px; padding: 5px;")
         logout_button.setCursor(QtCore.Qt.PointingHandCursor)
         logout_button.clicked.connect(self.logout_click)
         
-        user_button = QPushButton('Zalogowany użytkownik: Mariusz 👤')
+        user_button = QtWidgets.QPushButton('Zalogowany użytkownik: Mariusz 👤')
         user_button.setStyleSheet("color: black; font-weight: bold; font-size: 18px; background-color: yellow; border: 2px solid black; border-radius: 3px; font-family: Arial; margin: 10px; padding: 5px;")
         user_button.setCursor(QtCore.Qt.PointingHandCursor)
         
-        squares_layout = QHBoxLayout()
-        squares_layout.addWidget(self.todo_listwidget)
-        squares_layout.addWidget(add_button)
-        squares_layout.addWidget(edit_button)
-        squares_layout.addWidget(delete_button)
+        buttons_layout = QtWidgets.QHBoxLayout()
+        buttons_layout.addWidget(add_button)
+        buttons_layout.addWidget(edit_button)
+        buttons_layout.addWidget(delete_button)
         
-        bottom_layout = QHBoxLayout()
+        squares_layout = QtWidgets.QVBoxLayout()
+        squares_layout.addWidget(self.todo_listwidget)
+        squares_layout.addLayout(buttons_layout)
+        
+        bottom_layout = QtWidgets.QHBoxLayout()
         bottom_layout.addWidget(user_button)
         bottom_layout.addStretch()
         bottom_layout.addWidget(logout_button)
-        
-        news_layout = QHBoxLayout() 
-        news_layout.addWidget(self.news_textedit)
 
-        main_layout = QVBoxLayout()
+        main_layout = QtWidgets.QVBoxLayout()
         main_layout.addLayout(navigation_layout)
         main_layout.addLayout(squares_layout)
-        main_layout.addLayout(news_layout) 
         main_layout.addLayout(bottom_layout)
         self.setLayout(main_layout)
         
     def logout_click(self):
-        confirmation = QMessageBox(self)
+        confirmation = QtWidgets.QMessageBox(self)
         confirmation.setWindowTitle('Potwierdzenie')
-        confirmation.setIcon(QMessageBox.Question)
+        confirmation.setIcon(QtWidgets.QMessageBox.Question)
         confirmation.setText('Czy na pewno chcesz się wylogować?')
 
-        yes_button = confirmation.addButton('Tak', QMessageBox.YesRole)
+        yes_button = confirmation.addButton('Tak', QtWidgets.QMessageBox.YesRole)
         yes_button.setStyleSheet("background-color: green; color: white; font-weight: bold; font-size: 16px;")
 
-        no_button = confirmation.addButton('Nie', QMessageBox.NoRole)
+        no_button = confirmation.addButton('Nie', QtWidgets.QMessageBox.NoRole)
         no_button.setStyleSheet("background-color: red; color: white; font-weight: bold; font-size: 16px;")
 
         confirmation.exec_()
@@ -192,3 +182,6 @@ if __name__ == '__main__':
     window = CentralWindowMain()
     window.show()
     sys.exit(app.exec_())
+
+
+       
