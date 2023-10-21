a12f7e426d89fa4966ca6cf8ddc5ddf337550df6
Mariusz Perzynski <m.perzyk@interia.pl>
2023-10-21 17:08:50 +0200

    update rt elements

--- PROJECT-CODE/seasonoftrends.py
+++ PROJECT-CODE/seasonoftrends.py
@@ -1,5 +1,4 @@
 import random
-import requests
 from PyQt5 import QtWidgets, QtGui, QtCore
 
 class TrendSeasonalityWindow(QtWidgets.QDialog):
@@ -9,35 +8,30 @@ class TrendSeasonalityWindow(QtWidgets.QDialog):
         self.setWindowTitle("Trend i Sezonowość")
         self.setFixedSize(800, 600)
 
-        # Centrowanie okna na ekranie
-        self.move(QtWidgets.QDesktopWidget().availableGeometry().center() - self.rect().center())
-
-        # Kolor tła
-        self.setAutoFillBackground(True)
-        palette = self.palette()
-        palette.setColor(QtGui.QPalette.Window, QtGui.QColor(240, 240, 240))  # Jasnoszare tło
+        # Tło okna
+        background = QtGui.QImage("C:/Users/mperz/Desktop/MAIG WAREHOUSE/JPEGEIMAGE/New-World-niszczy-GPU.jpg")
+        background = background.scaled(self.size())
+        palette = QtGui.QPalette()
+        palette.setBrush(10, QtGui.QBrush(background))
         self.setPalette(palette)
 
         self.trend_label = QtWidgets.QLabel("Aktualny Trend - Top 10 Produktów:")
-        self.trend_label.setStyleSheet("font-size: 20px; font-weight: bold; color: #333;")
+        self.trend_label.setStyleSheet("font-size: 20px; font-weight: bold; color: red;")
 
         self.trend_text = QtWidgets.QTextEdit()
         self.trend_text.setReadOnly(True)
-        self.trend_text.setStyleSheet("font-size: 16px; background-color: white; color: #333;")
+        self.trend_text.setStyleSheet("font-size: 16px; background-color: #333; color: white;")
 
         self.seasonality_label = QtWidgets.QLabel("Sezonowość - Okres czasu:")
-        self.seasonality_label.setStyleSheet("font-size: 20px; font-weight: bold; color: #333;")
+        self.seasonality_label.setStyleSheet("font-size: 20px; font-weight: bold; color: red;")
 
         self.seasonality_text = QtWidgets.QTextEdit()
         self.seasonality_text.setReadOnly(True)
-        self.seasonality_text.setStyleSheet("font-size: 16px; background-color: white; color: #333;")
+        self.seasonality_text.setStyleSheet("font-size: 16px; background-color: #333; color: white;")
 
         self.fetch_data_button = QtWidgets.QPushButton("Pobierz Dane")
         self.fetch_data_button.setStyleSheet(
-            "font-size: 16px; font-weight: bold; background-color: #4CAF50; color: white; padding: 10px 20px;")
-
-        # Efekt zaokrąglenia przycisku
-        self.fetch_data_button.setStyleSheet("border-radius: 5px;")
+            "font-size: 16px; font-weight: bold; background: qradialgradient(cx: 0.5, cy: 0.5, fx: 0.5, fy: 0.5, radius: 1, stop: 0 #4CAF50, stop: 1 #388E3C); color: white; padding: 10px 20px; border: none; border-radius: 5px;")
 
         # Efekt cienia pod przyciskiem
         shadow = QtWidgets.QGraphicsDropShadowEffect()
@@ -80,12 +74,22 @@ class TrendSeasonalityWindow(QtWidgets.QDialog):
         import datetime
         today = datetime.date.today()
         one_year_later = today + datetime.timedelta(days=365)
-        return f"Okres:     {today.strftime('%d-%m-%Y')} - {one_year_later.strftime('%d-%m-%Y')}"
+        return f"Okres: {today.strftime('%d-%m-%Y')} - {one_year_later.strftime('%d-%m-%Y')}"
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication([])
     trend_seasonality_window = TrendSeasonalityWindow()
     trend_seasonality_window.exec_()
+
+
+
+
+
+
+
+
+
+
 
 
 
