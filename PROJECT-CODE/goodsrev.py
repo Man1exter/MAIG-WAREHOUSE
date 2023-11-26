00f911cdc6ffdf2f05a9ab8694de60c22d1e7e44
Mariusz Perzynski <m.perzyk@interia.pl>
2023-11-26 21:35:51 +0100

    remove empty space from label

--- PROJECT-CODE/goodsrev.py
+++ PROJECT-CODE/goodsrev.py
@@ -4,11 +4,12 @@ from PyQt5.QtWidgets import (
     QLabel, QLineEdit, QPushButton, QTableWidget, QTableWidgetItem,
     QGroupBox, QHeaderView, QDesktopWidget, QMessageBox
 )
-from PyQt5.QtGui import QPalette, QColor, QFont
+from PyQt5.QtGui import QPalette, QColor
 from PyQt5.QtCore import Qt
 
 class Product:
-    def __init__(self, ean, name, description, net_price, vat_rate, gross_price, quantity):
+    def __init__(self, index, ean, name, description, net_price, vat_rate, gross_price, quantity):
+        self.index = index
         self.ean = ean
         self.name = name
         self.description = description
@@ -41,23 +42,13 @@ class WarehouseWindow(QMainWindow):
 
         self.table = QTableWidget(self)
         self.table.setColumnCount(9)
-        self.table.setHorizontalHeaderLabels(["EAN", "Nazwa", "Opis", "Cena Netto", "VAT", "Cena Brutto", "Ilość", "", ""])
+        self.table.setHorizontalHeaderLabels(["INDEX", "EAN", "Nazwa", "Opis", "Cena Brutto", "VAT", "Cena Netto", "Ilość", ""])
         table_layout.addWidget(self.table)
 
         # Stylizacja nagłówków tabeli
         header = self.table.horizontalHeader()
-        for i in range(self.table.columnCount()):
-            header.setSectionResizeMode(i, QHeaderView.Fixed)
-            header.resizeSection(i, 150)  # Ustawiona szerokość kolumny
-
-        # Pogrubienie i zmiana koloru nagłówków tabeli
-        header_font = QFont()
-        header_font.setBold(True)
-        header_font.setPointSize(14)
-        header.setStyleSheet("font-size: 14px; background-color: #3498db; color: white;")
-        header.setDefaultAlignment(Qt.AlignCenter)
-        for i in range(self.table.columnCount()):
-            header.setFont(header_font)
+        header.setStyleSheet("QHeaderView::section { background-color: #3498db; color: black; font-size: 14px; }")
+        header.setSectionResizeMode(QHeaderView.Stretch)  # Maksymalna szerokość nagłówków
 
         main_layout.addWidget(table_group)
 
@@ -70,6 +61,9 @@ class WarehouseWindow(QMainWindow):
 
         form_layout = QFormLayout()
 
+        self.index_edit = QLineEdit(self)
+        form_layout.addRow("INDEX:", self.index_edit)
+
         self.ean_edit = QLineEdit(self)
         form_layout.addRow("EAN:", self.ean_edit)
 
@@ -79,16 +73,16 @@ class WarehouseWindow(QMainWindow):
         self.description_edit = QLineEdit(self)
         form_layout.addRow("Opis:", self.description_edit)
 
-        self.net_price_edit = QLineEdit(self)
-        self.net_price_edit.textChanged.connect(self.update_gross_price_edit)
-        form_layout.addRow("Cena Netto:", self.net_price_edit)
+        self.gross_price_edit = QLineEdit(self)
+        self.gross_price_edit.textChanged.connect(self.update_net_price_edit)
+        form_layout.addRow("Cena Brutto:", self.gross_price_edit)
 
         self.vat_rate_edit = QLineEdit(self)
         form_layout.addRow("VAT (%):", self.vat_rate_edit)
 
-        self.gross_price_edit = QLineEdit(self)
-        self.gross_price_edit.textChanged.connect(self.update_net_price_edit)
-        form_layout.addRow("Cena Brutto:", self.gross_price_edit)
+        self.net_price_edit = QLineEdit(self)
+        self.net_price_edit.textChanged.connect(self.update_gross_price_edit)
+        form_layout.addRow("Cena Netto:", self.net_price_edit)
 
         self.quantity_edit = QLineEdit(self)
         form_layout.addRow("Ilość:", self.quantity_edit)
@@ -98,9 +92,9 @@ class WarehouseWindow(QMainWindow):
         self.add_button.clicked.connect(self.add_product)
         form_layout.addRow(self.add_button)
 
-        # Pole do wyszukiwania produktu po nazwie
+        # Pole do wyszukiwania produktu po nazwie, indeksie i EANie
         self.search_edit = QLineEdit(self)
-        self.search_edit.setPlaceholderText("Wyszukaj produkt po nazwie")
+        self.search_edit.setPlaceholderText("Wyszukaj produkt po nazwie, indeksie lub EAN")
         self.search_edit.textChanged.connect(self.search_products)
         form_layout.addRow(self.search_edit)
 
@@ -160,17 +154,18 @@ class WarehouseWindow(QMainWindow):
                 font-size: 14px;
                 border: 2px solid #3498db;
                 border-radius: 8px;
-                padding: 8px;
+                padding: 12px;  /* Zwiększenie paddingu */
             }
         """)
 
     def clear_form(self):
+        self.index_edit.clear()
         self.ean_edit.clear()
         self.name_edit.clear()
         self.description_edit.clear()
-        self.net_price_edit.clear()
-        self.vat_rate_edit.clear()
         self.gross_price_edit.clear()
+        self.vat_rate_edit.clear()
+        self.net_price_edit.clear()
         self.quantity_edit.clear()
 
     def update_gross_price_edit(self):
@@ -192,6 +187,7 @@ class WarehouseWindow(QMainWindow):
             self.net_price_edit.clear()
 
     def add_product(self):
+        index = self.index_edit.text()
         ean = self.ean_edit.text()
         name = self.name_edit.text()
         description = self.description_edit.text()
@@ -201,31 +197,28 @@ class WarehouseWindow(QMainWindow):
         quantity = self.quantity_edit.text()
 
         try:
+            index = int(index)
             net_price = float(net_price)
             vat_rate = float(vat_rate)
             gross_price = float(gross_price)
             quantity = int(quantity)
 
-            product = Product(ean, name, description, net_price, vat_rate, gross_price, quantity)
+            product = Product(index, ean, name, description, net_price, vat_rate, gross_price, quantity)
 
             row_position = self.table.rowCount()
             self.table.insertRow(row_position)
-            self.table.setItem(row_position, 0, QTableWidgetItem(product.ean))
-            self.table.setItem(row_position, 1, QTableWidgetItem(product.name))
-            self.table.setItem(row_position, 2, QTableWidgetItem(product.description))
-            self.table.setItem(row_position, 3, QTableWidgetItem(f"{product.net_price:.2f}"))
-            self.table.setItem(row_position, 4, QTableWidgetItem(f"{product.vat_rate:.2f}"))
-            self.table.setItem(row_position, 5, QTableWidgetItem(f"{product.gross_price:.2f}"))
-            self.table.setItem(row_position, 6, QTableWidgetItem(str(product.quantity)))
-
-            edit_button = QPushButton("Edytuj", self)
-            edit_button.clicked.connect(lambda _, row=row_position: self.edit_product_row(row))
-            edit_button.setStyleSheet("background-color: #2ecc71; color: white; font-size: 14px; border: 2px solid #2ecc71; border-radius: 8px; padding: 8px;")
-            self.table.setCellWidget(row_position, 7, edit_button)
+            self.table.setItem(row_position, 0, QTableWidgetItem(str(product.index)))
+            self.table.setItem(row_position, 1, QTableWidgetItem(product.ean))
+            self.table.setItem(row_position, 2, QTableWidgetItem(product.name))
+            self.table.setItem(row_position, 3, QTableWidgetItem(product.description))
+            self.table.setItem(row_position, 4, QTableWidgetItem(f"{product.gross_price:.2f}"))
+            self.table.setItem(row_position, 5, QTableWidgetItem(f"{product.vat_rate:.2f}"))
+            self.table.setItem(row_position, 6, QTableWidgetItem(f"{product.net_price:.2f}"))
+            self.table.setItem(row_position, 7, QTableWidgetItem(str(product.quantity)))
 
             remove_button = QPushButton("Usuń", self)
             remove_button.clicked.connect(lambda _, row=row_position: self.remove_product_row(row))
-            remove_button.setStyleSheet("background-color: #e74c3c; color: white; font-size: 14px; border: 2px solid #e74c3c; border-radius: 8px; padding: 8px;")
+            remove_button.setStyleSheet("background-color: #e74c3c; color: white; font-size: 14px; border: 2px solid #e74c3c; border-radius: 8px; padding: 12px;")
             self.table.setCellWidget(row_position, 8, remove_button)
 
             # Zwiększenie wysokości rubryki o 20 pikseli
@@ -240,63 +233,17 @@ class WarehouseWindow(QMainWindow):
         search_text = self.search_edit.text().lower()
 
         for row in range(self.table.rowCount()):
-            name_item = self.table.item(row, 1)
-            if search_text in name_item.text().lower():
+            index_item = self.table.item(row, 0)
+            ean_item = self.table.item(row, 1)
+            name_item = self.table.item(row, 2)
+            if (
+                search_text in str(index_item.text()) or
+                search_text in ean_item.text().lower() or
+                search_text in name_item.text().lower()
+            ):
                 self.table.setRowHidden(row, False)
             else:
                 self.table.setRowHidden(row, True)
-
-    def edit_product_row(self, row):
-        self.selected_row = row
-        ean_item = self.table.item(row, 0)
-        name_item = self.table.item(row, 1)
-        description_item = self.table.item(row, 2)
-        net_price_item = self.table.item(row, 3)
-        vat_rate_item = self.table.item(row, 4)
-        gross_price_item = self.table.item(row, 5)
-        quantity_item = self.table.item(row, 6)
-
-        self.ean_edit.setText(ean_item.text())
-        self.name_edit.setText(name_item.text())
-        self.description_edit.setText(description_item.text())
-        self.net_price_edit.setText(net_price_item.text())
-        self.vat_rate_edit.setText(vat_rate_item.text())
-        self.gross_price_edit.setText(gross_price_item.text())
-        self.quantity_edit.setText(quantity_item.text())
-
-    def edit_product(self):
-        if self.selected_row == -1:
-            QMessageBox.warning(self, "Błąd", "Nie wybrano produktu do edycji.")
-            return
-
-        ean = self.ean_edit.text()
-        name = self.name_edit.text()
-        description = self.description_edit.text()
-        net_price = self.net_price_edit.text()
-        vat_rate = self.vat_rate_edit.text()
-        gross_price = self.gross_price_edit.text()
-        quantity = self.quantity_edit.text()
-
-        try:
-            net_price = float(net_price)
-            vat_rate = float(vat_rate)
-            gross_price = float(gross_price)
-            quantity = int(quantity)
-
-            product = Product(ean, name, description, net_price, vat_rate, gross_price, quantity)
-
-            self.table.setItem(self.selected_row, 0, QTableWidgetItem(product.ean))
-            self.table.setItem(self.selected_row, 1, QTableWidgetItem(product.name))
-            self.table.setItem(self.selected_row, 2, QTableWidgetItem(product.description))
-            self.table.setItem(self.selected_row, 3, QTableWidgetItem(f"{product.net_price:.2f}"))
-            self.table.setItem(self.selected_row, 4, QTableWidgetItem(f"{product.vat_rate:.2f}"))
-            self.table.setItem(self.selected_row, 5, QTableWidgetItem(f"{product.gross_price:.2f}"))
-            self.table.setItem(self.selected_row, 6, QTableWidgetItem(str(product.quantity)))
-
-            self.clear_form()
-
-        except ValueError:
-            QMessageBox.warning(self, "Błąd", "Niepoprawne wartości liczbowe!")
 
     def remove_product_row(self, row):
         confirmation = QMessageBox.question(self, "Potwierdzenie", "Czy na pewno chcesz usunąć ten produkt?", QMessageBox.Yes | QMessageBox.No)
@@ -311,6 +258,9 @@ if __name__ == "__main__":
     window = WarehouseWindow()
     window.show()
     sys.exit(app.exec_())
+
+
+
 
 
 
